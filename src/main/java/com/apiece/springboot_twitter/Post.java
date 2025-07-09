739a68923eb6ba6099666c57724c83c9a26a4a2e
glosona <sonaze28@gmail.com>
2025-07-09 18:07:37 +0900

    3. JPA and H2 Database

--- src/main/java/com/apiece/springboot_twitter/Post.java
+++ src/main/java/com/apiece/springboot_twitter/Post.java
@@ -1,17 +1,27 @@
 package com.apiece.springboot_twitter;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "posts")
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String content;
+
     private LocalDateTime createdAt;
 
     public void updateContent(String content) {
---
+++ src/main/java/com/apiece/springboot_twitter/repository/RepositoryConfig.java
@@ -0,0 +1,13 @@
+package com.apiece.springboot_twitter.repository;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public PostRepository postRepository(JpaPostRepository jpaPostRepository) {
+        return jpaPostRepository;
+    }
+}
